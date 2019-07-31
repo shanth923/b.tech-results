e077c656a66382db42ed886d93490759fa6bd452
BRN1907 <brn1907student@achiever-24.local>
2019-07-31 14:03:16 +0530

    Initial Commit

---
+++ b.tech results/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  b.tech results
+//
+//  Created by BRN1907 on 7/31/19.
+//  Copyright © 2019 BRN1907. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
